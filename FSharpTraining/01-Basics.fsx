58f661eed3ac031c075a8d89eddeb89810564241
ilmirajat <aplappi@gmail.com>
2012-05-30 01:00:54 +0300

    First version

---
+++ FSharpTraining/01-Basics.fsx
@@ -0,0 +1,326 @@
+﻿namespace FSharpTarinin.Basics
+module M1_MitaJaMiksiFSharp = 
+     (*
+     1. Mikä on F# ja miksi käyttäisin sitä?
+
+    Mitä F# on?
+        * F#  on multiparadigma ohjelmointi kieli, jonka painopiste on funktionaalisessa ohjelmointi paradigmassa.
+        * Yksi kolmesta Visual Studion mukana tulevista kielistä.
+    
+    Miksi?
+        - F# on ilmaisuvoimainen ja syntaksiltaan tiivis kieli. 
+            * Vaikka F# vahvasti tyypitetty kieli, sen monet rakenteet muistuttavat keveydessään dynaamisesti tyypitettyjä skripti kieliä (kuten Python ja Ruby).
+            * F# koodin rivimäärä on usein 2-5 alhaisempi kuin vastaavan C#-ratkaisun. Koodin luettavuus on samaa tasoa kuin C# tai Jave koodin - joskin eri asiat ovat 
+              helppolukuisia/helppolukuisia. F#:ssa on enemmän kryptisiä lynenteitä ja operaattoreja, siinä missä Javassa ja C#:ssa on enemmän avainsanoja ja idiomeja 
+              muistavaksi.
+        - C:n suorituskykyprofiili, eli vain hieman (=selvästi alle kymmenen kertaa) hitaampi kuin hyvin optimoitu C-koodi ja siten suunnilleen 
+              yhtä tehokas kuin Java ja C#. Dynaamiset, skriptikielet (kuten PHP, Python, ja Ruby) ovat 10-1000-kertaa optimoitua C-koodia hitaampia.
+              Ero korostuu laskentaintensiivisissä tehtävissä.
+        - Kaikki .NET frameworkin luokkakirjastot ovat käytetävissä. F# on .NET-kieli ja kääntyy CIL:ksi (kuten C# ja VB.NET; CIL = Common intermediate language, 
+              AKA Microsoft Intermediate lanuguage (MSIL)).
+
+    Missä? 
+        - F# soveltuu parhaiten Business logiikan ja datan käsittelyn toteuttamiseen. (Ks. Don Symen esitys F# 3.0: data, services, Web, cloud, at your fingertips 
+          http://channel9.msdn.com/Events/BUILD/BUILD2011/SAC-904T)
+        - F#:lla voi totettaa myös käyttöliittymäkerroksen ja tiedonesityskerroksen. WebSharper on kiinnostava esimerkki siitä, kuinka käyttöliittymän voi toteuttaa 
+          funktionaalisen ohjelmointi padigman hengessä (http://websharper.com/home).
+     
+     Esimerkki koodi?
+
+     Helpoin tapa suorittaa alla oleva koodi ja katsoa mitä tapahtuu on mennä ensimmäselle koodi riville ja painaa Alt-ä 
+     (Jos maalaat ja suoritat koodin (alt-enter), ota rivien alut mukaan, sillä sisennyksellä on F#:ssa oletuksena merkitystä).
+     
+     Esim. siirry alle olevalle riville "System.Console.Beep ()" ja paina alt+ä. Jos äänet ovat päällä pitäisi kuulua "beep".
+     *)
+     System.Console.Beep ()
+
+     // Alla oleva merkkijono tulostuu F# interaktiven tulosjonoon muoduossa: 
+     // val it : string = "Hello world"'
+     "Hello world"
+
+     // Klassinen hello world ikkunalla. 
+     // Apufunktiot show ja withControl helpottavat koodin suorittamista rivi riviltä
+     open System.Windows.Forms
+     let form = new Form()
+     form.Controls.Add(new Label(Text = "Hello world!"))
+     form.Show()
+
+     
+     // Klassinen "Hello, nimi!" muunnos.
+     let form = new Form()
+     let question = new Label(Dock = DockStyle.Top, Text = "Kuka olet?") 
+     let namefield = new TextBox(Dock = DockStyle.Top);
+     let hello = new Label(Dock = DockStyle.Top); 
+     namefield.KeyUp.Add(fun e-> hello.Text <- match namefield.Text with | "" -> "" | text -> "Hello, " + text + "!")
+     form.Controls.AddRange [|hello; namefield; question|]
+     form.Show()
+
+     // Interactiven sisällä voi muokata luotu lomaketta
+     let beep () = for i = 0 to 5 do System.Console.Beep()
+     namefield.KeyUp.Add(fun e-> hello.Text <- match namefield.Text with | "" -> "" | "test" | "Test" -> beep (); "Syötä oikea nimi!" | text -> "Hello, " + text + "!")
+
+module M2_TunnisteetJaLiteraalit = 
+    (*
+    2. Tunnisteet, primitiivityypit ja literaalit
+     - let
+     - vahvatyypistys
+     - F# interactive
+    *)
+
+    // F# tunnisteet esitellään let-avainsanalla, tunnisteet tyypin näkee viemällä hiiren kursori nimen päälle
+    // Koodin suurittaminen tapahtuu maalaamalla teksti ja painamalla alt+enter. (Valikoiden kautta saa aikaan saman.)
+    let x = 1 
+    let y = 1.0 
+    let str = "merkkijono"
+    let list = [1;2;3]
+    let array = [|1;2;3|]
+    let tupple = (1,2,3)
+    System.Console.Beep ()
+
+    // Oletuksena kaikki ovat vakioita. Muuttujat pitää erikseen merkata mutable arvain sanalla
+    // = merkki tarkoittaa yhtäsuuruutta aina paitsi asetettaessa tunnisteeseen arvo. Muuttujaan sijoitus tapahtuu <- operaattorilla
+    x = 1 // palauttaa arvon true. x:n arvo ei muutu.
+    let mutable z = 1
+    z = 1 // true
+    z <- 2 
+    z = 1 // false, koska arvo on nyt 2
+
+
+
+    // Interaktiven tulosvirtaan voi kirjoittaa komennolla printf printfn (jälkimmäinen kirjoittaa perään rivin vaihdon).
+    // printtaus metodi tekee tyyppi tarkastuksen
+    // %d = kokonaisluku
+    // %f = liukuluko
+    // %s = merkkojono
+    // %O = objekti 
+    // %A = taulukko"
+    printfn "%d %f %s %O %A" x y str list array 
+
+module M3_Funktiot = 
+    (*
+    3. Funktiot
+     - funtiot ovat ensimmäisen luokan kansalaisia
+     - currying
+     - rekursio
+    *)
+
+    // Funtitiot esitellään täsmälleen samoin
+    // function tyyppi on int->int->int
+    let plus x y = x + y
+    plus 1 2
+
+    // Yksi kiinnostavimmista ominaisuksista on funktioiden ketjutus
+    // Idean hahmottaa helpoten sulkeistamlla. Kun ensimmäisen int:n korvaa 5:llä fyypiksi (func 5)->int->int.
+    let lisää_viiteen = plus 5
+
+    // Sijoittamalla ensimmäiseksi arvoksi 5 funktioon tyyppiä int->int, jäljelle jää int, joka itse asiassa saadaan suorittamalla funktio loppuun asti.
+    lisää_viiteen 5
+
+    // Rekursiivisen function esittelyyn pitää lisätä rec (mahdollisesti pitkälti F# REPL kääntäjän takia). Rekursiolla ei ole vaikutusta funktion tyyppiin.
+    // Huomaa funktion voi määritellä myös toisen funkion sisään. Rekursiivisten functioden osalta tämä on näppärä sääntö.
+    let rec factorialPlus x =
+        let rec factorialRec (x:int64) acc =
+            if x > 1L then factorialRec (x - 1L) (x + acc)
+            else acc
+        factorialRec x 1L    
+
+    factorialPlus 5L // 5 + 4 + 3 + 2 + 1 = 15
+    factorialPlus 20000000L // val it : int64 = 200000010000000L
+
+    /// Käytettäessä häntärekursiota F# hanskaa tilanteen jossa pino vuotaisi muuten yli (Stackoverflow). Häntärekursioksi kutsutaan rekursion erityistapausta, 
+    /// jossa rekursiivisen kutsun paluuarvosta tulee ilman lisäoperaatioita kutsuvan instanssin paluuarvo. Tämä tarkoittaa sitä, että rekursioiden "purkautuessa" 
+    // ei ole enää mitään tekemistä.
+    let rec factorialPlusNonTail (x:int64) = 
+        if(x > 1L) then
+            x + (factorialPlusNonTail (x - 1L))
+        else x
+    factorialPlusNonTail  5L // 5 + 4 + 3 + 2 + 1 = 15
+    factorialPlusNonTail  20000000L // Process is terminated due to StackOverflowException.
+
+module M4_PatternMatchin = 
+    (*
+    4. Pattern matching vaihtoehtona if-elselle 
+     - swich case rakenne tehtynä oikein
+     - F#:ssa erittäin ilmoisuvoimainen rakenne
+     - http://msdn.microsoft.com/en-us/library/dd547125.aspx
+    *)
+
+    // Pattern matchingiä voi käyttää samaan tapaan kuin if-else sykliä voisi. 
+    // Lopputulos on usein helppo lukuisempi ja tiiviimpi.
+    //          |
+    //     1    |     2
+    //          |
+    //  ---------------------
+    //          |
+    //     3    |     4
+    //          |
+    let resolveQuartile point =
+        match point with
+        | (x,y) when x >= 0 && y >= 0 -> 2
+        | (x,y) when x < 0 && y >= 0 -> 1
+        | (x,y) when x >= 0 && y < 0 -> 4
+        | _ -> 3
+
+    resolveQuartile (1,2)
+    resolveQuartile (-1,2)
+    resolveQuartile (1,-2)
+    resolveQuartile (-1,-2)
+
+    //  F# Pattern matching tukee kymmenkuntaa eri "hahmontunnistus-kaavaa". Esimerkiksi parametrina annettun objektin tyyppi.
+    // Esimerkki 1
+    let format (o: obj) =
+        match o with
+        | :? System.DateTime as day -> "Päivämäärä: " + day.ToString("dd.MM")
+        | :? System.Int32 as i -> "Kokonaisluku: " + i.ToString()
+        | _ -> "Jokin muu objekti"
+
+    format (System.DateTime.Now)
+    format 1
+    format 1.0
+
+module M5_TyypitJaObjektiOrientoitunutOhjelmointi = 
+    (*
+    5. Tyypit 
+    - Luokat ja rajapinnat
+    - Vaihtoehdot luokille ja rajapinnoille
+        - Tietue tyyppi (Record type)
+        - Jäsennetty yhdiste (Discriminate union) ja Pattern matching
+        - Object expression
+    *)
+
+    // F# on täysiverinen objekti orientoitunut ohjelmointi kieli, 
+    // joskin sen rakenteet kannustavat hyödyntämään muunlaisia rakenteita
+    // luokka. F# ei tee.
+
+    // Luodaan ensin vertailun vuoksi validointi logiikka OOP paradigman mukaisesti hyödyntäen rajapintoja ja luokkia.
+    type IValidateInt = 
+        abstract Validate: int -> bool 
+
+    type LessThanValidator (max) =
+        let max = max
+        interface IValidateInt with 
+            member x.Validate (intToValidate) = intToValidate < max
+        
+    type GreaterThanValidator (min) =
+        let min = min
+        interface IValidateInt with 
+            member x.Validate (intToValidate) = intToValidate > min
+
+    let lessThan10Validator = new LessThanValidator(10) :> IValidateInt
+    let moreThan5Validator  = new GreaterThanValidator(5) :> IValidateInt
+    lessThan10Validator.Validate 9 // true
+    lessThan10Validator.Validate 12 // false
+    moreThan5Validator.Validate 2 // false
+    moreThan5Validator.Validate 8 // true
+
+    // Rajanpinnan käyttö tekee rakenteesta asteen joustavammat, mutta koodia pahaisen ominaisuuden määrittylyyn 
+    // tarvitsee kirjoittaa enemmän kuin itse sovellus logiikkan monimutkaisuus soisi. 
+    // F# jäsennelty unioni (discrimate union) tuo ratkaisun tähän. Oheinen ratkaisu on huomattavist monipuolisempi mutta ei juurikaan monimutkaisempi tai pidempi.
+    type ValidateInt =
+        | GreaterThan of int
+        | LessThan of int
+        | Predicate of (int -> bool)
+        | And of ValidateInt * ValidateInt
+        | Or of ValidateInt * ValidateInt
+        member x.Validate intToValidate =
+            match x with
+            | GreaterThan max -> intToValidate > max
+            | LessThan min -> intToValidate < min
+            | Predicate validationFunction -> validationFunction intToValidate
+            | And (validator1, validator2) -> (validator1.Validate intToValidate) && (validator2.Validate intToValidate)
+            | Or (validator1, validator2) -> (validator1.Validate intToValidate) || (validator2.Validate intToValidate)
+
+    let lessThan10Validator = LessThan  10 
+    let moreThan10Validator  = GreaterThan  10 
+    lessThan10Validator.Validate 9 // true
+    lessThan10Validator.Validate 12 // false
+    moreThan10Validator.Validate 9 // false
+    moreThan10Validator.Validate 12 // true
+
+    // Ja sitten jotain ihan muuta. Joko parillinen ja yli 10 tai pariton ja alle kymmenen
+    let complexValidator1 = 
+        let isOddValidator = ValidateInt.Predicate (fun x -> (x % 2) = 1)
+        let isEvenValidator = ValidateInt.Predicate (fun x -> (x % 2) = 0)
+        Or (And ((GreaterThan 10), isEvenValidator), (And ((LessThan 10), isOddValidator)))  
+    complexValidator1.Validate 9 // true
+    complexValidator1.Validate 13 // false
+    complexValidator1.Validate 8 // false
+    complexValidator1.Validate 12 // true
+
+    // Ratkaisu toimii mutta syntaksin edellyttämä sulkuhässäkästä on erittäin vaikea lukuinen.
+    // F# sallii luettavuutta helpottavien prefix ja infiksi opreattootien luonnin.
+    // Harmittavasti CLI (Common Language Infrastructure) ei anna määrittää && tai || operaattoreja 
+    // jotka palauttavat jotain muuta kuin booleanin. +& ja +| ovat hieman kryptisiä nimiä. 
+
+    // On myös syytä huomata että F# sallii luokan määrittämisen ja laajentamisen partiaalisesti jopa sen jälkeen 
+    // kun siitä on luotu instansseja. Tässä ValidateInt luokkaa laajennetaan "lennosta" kahdella staatisella jäsenellä.
+    // Koodi on kuitenkin edelleen vahvasti tyypitettyä, sillä tätä tyyppi laajennusta ei hyödynnetä tätä ennen.
+    type ValidateInt with
+        static member (+&) (fst:ValidateInt, snd : ValidateInt) =
+            ValidateInt.And (fst,snd)
+        static member (+|) (fst:ValidateInt, snd:ValidateInt) =
+            ValidateInt.Or (fst,snd)
+
+    let complexValidator2 = 
+        let isOddValidator = ValidateInt.Predicate (fun x -> (x % 2) = 1)
+        let isEvenValidator = ValidateInt.Predicate (fun x -> (x % 2) = 0)
+        (GreaterThan 10 +& isEvenValidator) +| (LessThan 10 +& isOddValidator)
+    complexValidator2.Validate 9 // true
+    complexValidator2.Validate 13 // false
+    complexValidator2.Validate 8 // false
+    complexValidator2.Validate 12 // true
+
+module M6_LoopitJaListaOperaatiot = 
+    (*
+    5. Loopit ja lista operaatiot
+     - for
+     - komentojen putkitus (pipelines)
+     - Seq ja List -operaatiot
+    *)
+
+    // F# sallii klassiseten ohjelmointi kielten for loopit
+    let simpleList = [1;2;3]
+    for i in simpleList  do
+        printfn "Jee %d" i
+
+    // Usein for loopeja tehokkaampaa ja elegantimpaa on putkittaa komentoja. |> operaattorilla
+    // (Yksinkertaisissa tapauksissa tosin putkitus ei tosin selvennä juurikaan koodia.)
+    list |> List.iter (printfn "Jee %d") 
+
+    // For looppi sopii parhaiten listojen generointiin
+    open System
+    let firstday2012 = new System.DateTime(2012,1,1)
+    let year2012 = seq {for i in 0.0 .. 356.0 -> firstday2012.AddDays(i)}
+
+    // Kun loopin logiikka monimutkaistuu, komentojen putkitus alkaa selkeyttämään koodia enevässä määrin.
+    // Funktionaalinen ohjelmointi paradikma on vahvimmillaan nimen omaan monimutkaisten ongelmian kanssa.
+    year2012 
+    |> Seq.filter (fun day -> day.DayOfWeek = DayOfWeek.Friday && day.Day = 13)
+    |> Seq.iter (printfn "%O")
+
+    // Tai jotain vielä villinpään näytä perjantait ja milloin vastaava päivämäärä on perjantai seuraavan kerran.
+    let rec findNextSameFriday13 (day : DateTime) =
+        let next = new DateTime(day.Year + 1, day.Month, day.Day)
+        match next.DayOfWeek with
+        | DayOfWeek.Friday -> next
+        | _ -> findNextSameFriday13 next
+    
+    year2012 
+    |> Seq.filter (fun day -> day.DayOfWeek = DayOfWeek.Friday && day.Day = 13)
+    |> Seq.map (fun day -> (day, findNextSameFriday13 day))
+    |> Seq.iter(fun (day2012, dayN) ->  (printfn "%O on seuraavan kerran perjantai 13. vuonna %d" day2012 dayN.Year))
+
+    // F# sallii äärettömät sekvuvenssit. Seuraava funktio etsii ensimäisen perjantaina joka on kolmastoista päivä ja jolle 
+    // vuosi * kuukaus * päivä on suurempi kuin 1 000 000. Esim. jos 13.12.2012 olisi perjantai vuoden, kuukauden ja päivän tulo olisi 313872
+
+    // 1. Ensin koodia selventävä apufunktio seuraavan kolmannentoista päivän hakemiseen. 
+    let rec findNextFriday (day : DateTime) =
+        let next = day.AddDays(1.0)
+        match (next.Day, next.DayOfWeek) with
+        | 13, DayOfWeek.Friday -> next
+        | _ -> findNextFriday next
+
+    // 2. Tämän jälkeen luodaan ääretön sekvenssi perjantai kolmastoista päiviä
+    let fri13seq = Seq.unfold (fun state -> Some(state, (findNextFriday state))) (new DateTime(2012,1,13))
+    // 3. Lopuksi iteroidaan sekvenssiä läpi kunnes ehdot täyttävä päivä löytyy.
+    fri13seq |> Seq.skipWhile (fun day -> day.Year * day.Month * day.Day < 1000000) |> Seq.head
---
+++ FSharpTraining/02-AsyncExample1.fsx
@@ -0,0 +1,173 @@
+﻿// namespace FSharpTrainin
+#r "System.Net.dll"
+(* 
+Esimerkki perustuu Tomas Petricekic koodiframenttiin, joka löytyy osoitteesta http://fssnip.net/6e
+
+Esimerkin idea on demonstroida seuraavia ominaisuuksia:
+1) Luokkien laajentaminen ja hyödyntäminen ilman periyttämistä tai muita vastaavia raskaita ratkaisuja ongelmaan. 
+2) Async tyyppi, AsynBuilder ja niiden käyttö
+*)
+module HttpListenerHelpers = 
+    open System
+    open System.IO
+    open System.Net
+    open System.Threading
+    open System.Collections.Generic
+
+    // Alla HttpListener ja WebClient luokiin laajenentaan tuki F#:n Async tyypille. 
+    // Näistä WebClient luokan laajennus on vaikea selkoisempi. Olennaisesti 
+    // Async.FromContinuations tuottaa Async luokasta sellaisen instanssin joka kuuntelee kolmea asyncronista tapahtumaa:
+    // - Tuli vastaus yritykseen ladata url. Ei ongelma
+    // - Jotain meni pieleen.
+    // - Lataus peruutettiin.
+    // 
+    // Async luokka toimii eräänlaisena proxynä WebClientin DownloadDataComplited eventille. 
+    //
+    // Ensimmäisessä tapauksessa luodaan Async proxy HttpListenerin BeginGetContext ja EndGetContext delegaatille. 
+    // Käytännössä tämä sallii sivukyselyn käsittelyn syncronisesti ja vastuksen kirjoittamisen HttpResponsen 
+    // tulostevirtaan kunhan sellainen web clientilta saapuu.
+    type System.Net.HttpListener with
+        member x.AsyncGetContext() = 
+            Async.FromBeginEnd(x.BeginGetContext, x.EndGetContext)
+
+    type System.Net.WebClient with
+        /// Asynchronously downloads data from the 
+        member x.AsyncDownloadData(uri) = 
+            Async.FromContinuations(fun (cont, econt, ccont) ->
+                x.DownloadDataCompleted.Add(fun res ->
+                    if res.Error <> null then econt res.Error
+                    elif res.Cancelled then ccont (new OperationCanceledException())
+                    else cont res.Result)
+                x.DownloadDataAsync(uri) )
+
+    // Itseäni on rasittanut se, C# extension metodeilla pystyy luomaan vain instanssin metodeja mutta ei staattisia metodeja. F#:ssa ei ole tätä rajoitusta.
+    type System.Net.HttpListener with 
+        /// Starts an HTTP server on the specified URL with the
+        /// specified asynchronous function for handling requests
+        static member Start(url, f) = 
+            let tokenSource = new CancellationTokenSource()
+            Async.Start
+                ((async { 
+                    use listener = new HttpListener()
+                    listener.Prefixes.Add(url)
+                    listener.Start()
+                    while true do 
+                        let! context = listener.AsyncGetContext()
+                        Async.Start(f context, tokenSource.Token)}),
+                    cancellationToken = tokenSource.Token)
+            tokenSource
+
+      /// Starts an HTTP server on the specified URL with the
+      /// specified synchronous function for handling requests
+        static member StartSynchronous(url, f) =
+            HttpListener.Start(url, f >> async.Return) // Location where the proxy copies content from
+
+
+module ASyncLister = 
+    open System
+    open System.IO
+    open System.Net
+    open HttpListenerHelpers
+    let mutable token = null
+    let mutable mirrorRoot = null
+    
+    let getProxyUrl (ctx:HttpListenerContext) = 
+        Uri(mirrorRoot + ctx.Request.Url.PathAndQuery)
+    
+    // Funktio käsittelee poikkeukset asyncronisesti
+    // Tämä funktio kääntyy muotoon 
+    // [1] AsyncBuilder.Delay(fun () -> 
+    //    use wr = new StreamWriter(ctx.Response.OutputStream)
+    //     [clip]
+    //   ctx.Response.Close()
+    // Kun tapahtuu virhe:
+    // do! asyncHandleError ctx e 
+    // kääntyy muotoon 
+    //
+    // AsyncBuilder.Bind([1], [seurava async operaatio]) (ks. alla)
+    //
+    // Bind metodi suorittaa operaation jonka Delay on lykännyt. 
+    let asyncHandleError (ctx:HttpListenerContext) (e:exn) = async {
+       use wr = new StreamWriter(ctx.Response.OutputStream)
+       wr.Write("<h1>Request Failed</h1>")
+       wr.Write("<p>" + e.Message + "</p>")
+       ctx.Response.Close() }
+
+    // AsyncBuilder mahdollistaa asynkronisten opreatioitioiden putkittamisen 
+    // minimi määrällä pään rapsutusta. Tässä kokonaisuudessa putkitetaan kolme
+    // asynkronista operaatiota. 
+    // 1) Sivu pyyntö tulee kun on tullakseen. Kun ase tulee aloitetaan tämän koodi blokin suoritus.
+    // 2) Ladataan asynrkonisesti data verkosta.
+    // 3a) kun lataus byte[] on käytetävissä kirjoitetaan data asynknoisesti kuunneltavan konteksin 
+    // tulostevirtaan 
+    // 3b) jos homma meni kaikki meni pieleen niin kirjoitetaan virhe ja tuli poikkeus. Kirjoitetaan virhe tulosvirtaan.
+    // "let! data = [...]" ja "do! ctx.Response.OutputStream.AsyncWrite(data)" putkittaa opreaatio automaattisesti siten että 
+    // jälkimmäinen suoritetaan vasta kun ensimmäinen palauttaa arvon. Sovellus ei jää odottamaan että näin tapahtuu.
+    // saman voisi toteuttaa myös ilman syntaktista sokeria ja lopputulos näyttäisi tämän tapaiselta.
+    // let async1 = new AsyncBuilder()
+    // [...]
+    // async1.Delay(fun () ->
+    //    let wc = new WebClient()
+    //    try
+    //      async1.Bind(wc.AsyncDownloadData(getProxyUrl(ctx)),(fun data ->
+    //           async1.Bind(ctx.Response.OutputStream.AsyncWrite(data),(fun () ->
+    //                  async1.Return()))
+    //    catch e -> 
+    //         async1.Bind(asyncHandleError ctx e, async1.Return()
+    //
+    // F#:n async laskentailmaus (computational expression) tekee asynknonisesta ohjelmoinnista huomattaasti 
+    // helpompaa ja asynkronisesta koodista huomattaasti helppolukuisempaa ja tiiviimpää.
+    let asyncHandleRequest (ctx:HttpListenerContext) = async {
+        let wc = new WebClient()
+        try
+            let! data = wc.AsyncDownloadData(getProxyUrl(ctx))
+            do! ctx.Response.OutputStream.AsyncWrite(data) 
+        with e ->
+            do! asyncHandleError ctx e }
+    
+    let StartMirroring (url) =
+        // Start HTTP proxy that handles requests asynchronously
+        mirrorRoot <- url
+        token <- HttpListener.Start("http://localhost:8080/", asyncHandleRequest)
+    let Stop () = 
+        if (token <> null) then token.Cancel()
+
+module SyncVersion =
+    open System
+    open System.IO
+    open System.Net
+    open HttpListenerHelpers
+    let mutable token = null
+    let mutable mirrorRoot = null
+    
+    let getProxyUrl (ctx:HttpListenerContext) = 
+        Uri(mirrorRoot + ctx.Request.Url.PathAndQuery)
+    // Handle exception - generate page with message
+    let handleError (ctx:HttpListenerContext) (e:exn) =
+       use wr = new StreamWriter(ctx.Response.OutputStream)
+       wr.Write("<h1>Request Failed</h1>")
+       wr.Write("<p>" + e.Message + "</p>")
+       ctx.Response.Close()
+
+    let handleRequest (ctx:HttpListenerContext) =
+        let wc = new WebClient()
+        try
+          let data = wc.DownloadData(getProxyUrl(ctx))
+          ctx.Response.OutputStream.Write(data, 0, data.Length)
+          ctx.Response.Close()
+        with e ->
+         handleError ctx e
+ 
+    let StartMirroring (url) =
+        // Start HTTP proxy that handles requests asynchronously
+        token <- HttpListener.StartSynchronous("http://localhost:8080/", handleRequest)
+    let Stop () = 
+        if (token <> null) then token.Cancel()
+
+(*
+ASyncLister.StartMirroring "http://msdn.microsoft.com"
+ASyncLister.Stop()
+
+SyncVersion.StartMirroring "http://msdn.microsoft.com"
+SyncVersion.Stop()
+*)
