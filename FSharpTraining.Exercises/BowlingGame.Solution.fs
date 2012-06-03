20a64ee17a9cfb81c4b882c398f7c324a38f824b
ilmirajat <aplappi@gmail.com>
2012-06-03 20:43:51 +0300

    Small tweaks to bowling ball example

--- FSharpTraining.Exercises/BowlingGame.Solution.fs
+++ FSharpTraining.Exercises/BowlingGame.Solution.fs
@@ -1,20 +1,15 @@
 ﻿namespace FSharpTraining.Exercise 
+// Alla on kaksi referenssi ratkaisua ongelmaan.
 
-// Referenssi toteutus 1
-// Toteutus noudattelee Martin & Martinin ratkaisua.
+// Näistä ensimmäinen on pyritty rakentamaan mahd. pitkälti funtonaalisen ohjelmointiparadigman hengen mukaisesti 
+// valitettavasti arvoa palauttamaton Add metodi tekee täysin sivuvaikutuksettoman ratkaisun kirjoittamisen 
+// mahdottomaksi. Esimerkissä on hyödynnetty laajasti joko-tai-luokkia (discriminate union) sekä 
+// hahmontunnistusta (pattern matching).
+
+// Myös toisessa ratkaisussa ratkaisua on viety selvästi kohti funtionaalista ohjelmointi paradigmaa karsimalla 
+// alku peräisen ratkaisun muokattavissa olevia kenttiä (mutable fields) minimiin. Täysin olio orientoitunut verrokin löytyy
+// Martin & Martinin kirjasta Agile Patterns and Pratices in C#, luvusta 6:
 // Ks. http://my.safaribooksonline.com/book/programming/csharp/0131857258/a-programming-episode/ch06lev1sec1.
-
-// Koodia on hieman siistitty F# henkisemmäksi. Suurin muutos on Scorer luokan muuttaminen immutableksi. 
-// Pääsääntöisesti se on uskollinen Martin & Martinin ratkaisulle.
-
-// Usein on huono idea lähteä miettimään F#-ratkaisua suoraa C#-koodin pohjalta: tämä johtaa samanlaiseen koodiin
-// kuin jos VB 6 koodia kääntäisi C#-koodiksi. Siitä huolimatta suurin osa nykypäivän koodista on C#-koodia.
-// Pisteiden laskenta noudattaa logiikkaa joka sallii sivuvaikutukset (mutable).
-
-
-
-
-
 
 
 module FuntionalSolution =
@@ -34,25 +29,23 @@ module FuntionalSolution =
             | _ -> 0
         static member Add (pins:int) (scorecard:Frame list) =
             let updateSparesAndStrikes (scorecard:Frame list) : Frame list  =
-                let completeItem (itemToComplate) = 
-                    match itemToComplate with 
+                let completeItem (itemToComplete) = 
+                    match itemToComplete with 
                     | CompleteAndIncludeNextOne total -> CompleteFrame (total+pins)
                     | CompleteAndIncludeNextTwo total -> CompleteAndIncludeNextOne (total+pins)
-                    | _ -> itemToComplate
+                    | _ -> itemToComplete
                 scorecard |> List.map completeItem
             let addThrow (scorecard:Frame list) =
                 let setLastFrame last tail (scorecard:Frame list) = 
                     let round = scorecard |> List.length
                     match last with
-                    // Last throw after score or strike in the last round
+                    // Last throw after spare or strike in the last round
                     | LastRoundIncludeNextOne (total) -> CompleteFrame (total+pins)::tail
                     // Last round spare
-                    | FirstThrow (lastThrow ) when pins + lastThrow = 10 && round = 10 -> 
-                        (LastRoundIncludeNextOne (10))::tail
-                    // Spare
-                    | FirstThrow (lastThrow) when pins + lastThrow = 10 && round <> 10 -> 
-                        (CompleteAndIncludeNextOne (10))::tail                    
-                    // last round strike or score
+                    | FirstThrow (lastThrow ) when pins + lastThrow = 10 && round = 10 -> (LastRoundIncludeNextOne (10))::tail
+                    // normal Spare
+                    | FirstThrow (lastThrow) when pins + lastThrow = 10 -> (CompleteAndIncludeNextOne (10))::tail                    
+                    // last round strike
                     | _ when pins  = 10 && round = 9 -> (LastRoundIncludeNextOne (10))::scorecard
                     // normal strike
                     | _ when pins = 10 -> (CompleteAndIncludeNextTwo (10))::scorecard
@@ -65,14 +58,13 @@ module FuntionalSolution =
                     | last::tail -> setLastFrame last tail scorecard
             scorecard |> updateSparesAndStrikes |> addThrow
 
-    type Game (list:Frame list) =
-        let mutable scorecard  = list
-        new() = Game (List.Empty)
+    type Game () =
+        let mutable scorecard : Frame list = []
         interface IGame with
             member x.Score = scorecard |> List.sumBy (fun frame -> frame.FrameScore)
-            member x.Add(pins : int) = 
-                scorecard <- Frame.Add pins scorecard
+            member x.Add(pins : int) =  scorecard <- Frame.Add pins scorecard
             member x.ScoreForFrame(theFrame) =  
+                // Notice that in scorecard Framelist the last played frame is the frist one.
                 scorecard |> List.rev |> List.toSeq |> Seq.take theFrame |> Seq.sumBy (fun f -> f.FrameScore)
              
     module Tests = 
@@ -82,9 +74,30 @@ module FuntionalSolution =
             inherit BowlingTestsBase() 
             override x.newGame ()  = (new Game()):>IGame
 
+// Referenssi toteutus 2
 
+// Toteutus noudattelee Martin & Martinin ratkaisua. Koodia on hieman siistitty F# henkisemmäksi. Suurin muutos on 
+// Scorer luokan muuttaminen ei-muutettavaksi (immutableksi). Yhtäkaikki pääsääntöisesti se on uskollinen 
+// Martin & Martinin ratkaisulle.
 
+// Usein on huono idea lähteä miettimään F#-ratkaisua suoraa C#-koodin pohjalta: tämä johtaa samanlaiseen koodiin
+// kuin jos VB 6 koodia kääntäisi C#-koodiksi. Siitä huolimatta suurin osa nykypäivän koodista on C#-koodia.
+// Pisteiden laskenta noudattaa logiikkaa joka sallii sivuvaikutukset (mutable), mutta toisaalta muuttamatta rajapintaa 
+// Add-metodin osalta täysin sivuvaikutukseton, ei-muutettavissa oleviin rakenteisiin perustuva ratkaisu on mahdoton.
 
+// Huomaa että tässä tapauksessa mahdollisimman pitkälti funktionaalisen paradigman mukaisesi rakennettu ratkaisu 
+// ei ole mitenkään automaattisesti parempi kuin semi-funktionaalinen ajatellen koodin tiiviyttä ja luettavuutta
+// molemmat ratkaisut ovat alkuperäistä C# toteutusta jonkin verran selkeämpiä ja tiiviimpiä.
+
+// Kannustan vertailemaan ratkaisua ja pohtimaan kumpi ratkaisusta on selkämpi ja luettavampi. Muilta osin ratkaisut ovat pitkälti 
+// toisiaan vastaavat: Ne ovat suunnilleen yhtä helposti laajennettavia ja ylläpidettäviä jne.
+ 
+// Oletan että olio-ohjelmointi taustalla ja rutiinilla varustetusta funtinaaliseen ohjelmointiin enintään pintapuolisesti 
+// tutustuneet pitävätä seuraavaa esimerkkiä hieman selkeämpänä. Tähän on pari selkeää syystä:
+// 1) Käyttämäni funktionaalisen ratkaisun kaksi keskeisintä tietorakennetta (joko-tai-luokat ja hahmontunnistut) ovat outoja C# 
+// rakenteisiin tottuneille.
+// 2) Olio-ohjelmoinnin rakenteisiin tottuneet eivät enää osaa kiinnittää huomiota kaikkiin sellaisiin rakenteisiin, jotka 
+// tekevät siitä sekavahkon ja hahkalasti seurattavan. 
 module AlmostOriginalImplementation =
     open Contract 
     type Scorer =
